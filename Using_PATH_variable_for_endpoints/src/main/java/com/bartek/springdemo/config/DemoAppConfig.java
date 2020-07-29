36c165709bc7b297578ae73fc75c88e2318b1cd2
Bartek Walczak <50672367+bl4keee@users.noreply.github.com>
2020-07-29 07:09:55 +0200

    Initial Commit

---
+++ Using_PATH_variable_for_endpoints/src/main/java/com/bartek/springdemo/config/DemoAppConfig.java
@@ -0,0 +1,12 @@
+package com.bartek.springdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.bartek.springdemo")
+public class DemoAppConfig {
+
+}
